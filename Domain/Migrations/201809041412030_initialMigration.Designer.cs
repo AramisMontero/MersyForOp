799b2b07c97c39dd75632ba5c093331752cf0ced
yean urdaneta <ycuz_o_matic@hotmail.com>
2018-09-19 17:21:55 -0400

    generacion de las migraciones. falta probar por error en las views dentro de configurations que no existen fisicamente.

--- Domain/Migrations/201809041412030_initialMigration.Designer.cs
+++ Domain/Migrations/201809041412030_initialMigration.Designer.cs
@@ -7,13 +7,13 @@ namespace Domain.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class init : IMigrationMetadata
+    public sealed partial class initialMigration : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(init));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(initialMigration));
         
         string IMigrationMetadata.Id
         {
-            get { return "201808251439168_init"; }
+            get { return "201809041412030_initialMigration"; }
         }
         
         string IMigrationMetadata.Source
